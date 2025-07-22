fd80067ce731f01516f188b72819d976ffc944e1
Anil Paul <anilnpaul@gmail.com>
2025-07-22 13:00:27 +1000

    clean up

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white">
       <div className="container mx-auto px-4 py-4 text-center">
-        <p>@copy; 2025 song</p>
+        <p>{"\u00A9"} 2025 song</p>
       </div>
     </footer>
   );
