32b6c7bd1005c9b01539b247182e55f62a305c2d
Anil Paul <anilnpaul@gmail.com>
2025-07-22 12:44:23 +1000

    feat(products): Build ProductCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { Header } from "./components/Layout/Header";
 import { Footer } from "./components/Layout/Footer";
-
+import { ProductCard } from "./components/ProductCard/ProductCard";
+import type { Product } from "./types";
+import productsData from "./assets/products.json";
+import { useState } from "react";
 const App = () => {
+  const [products, setProducts] = useState<Product[]>(
+    productsData as Product[]
+  );
+  const randomIndex = Math.floor(Math.random() * productsData.length);
+  const sampleProduct = products[randomIndex - 1];
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
       <Header />
-      <main className="flex-grow container mx-auto p-4">Product List</main>
+      <main className="flex-grow container mx-auto p-4">
+        <ProductCard product={sampleProduct} />
+      </main>
       <Footer />
     </div>
   );
---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,21 @@
+import type { Product } from "../../types";
+
+type ProductCardProps = {
+  product: Product;
+};
+
+export const ProductCard = ({ product }: ProductCardProps) => {
+  const imageUrl = `/images/products/${product.productImage}`;
+
+  return (
+    <div className="bg-white border rounded-lg shadow-md p-4 flex flex-col text-center">
+      <img
+        src={imageUrl}
+        alt={product.productName}
+        className="w-full h-48 object-contain mb-4"
+      />
+      <h3 className="text-lg font-semibold flex-grow">{product.productName}</h3>
+      <p className="text-xl font-bold text-gray-900 mt-2">{product.price}</p>
+    </div>
+  );
+};
